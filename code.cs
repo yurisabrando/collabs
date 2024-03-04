863c6d75ce363c44ca0c1873c44bc931b8ca543b
Edmund Gerhardt <162111400+yurisabrando@users.noreply.github.com>
2024-03-04 11:32:40 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,19 @@
+    public static (int[], int, int) LoadBitmap(string filename)
+    {
+        using var image = Image.Load<Bgra32>(filename);
+        int width = image.Width, height = image.Height;
+        int[] result = new int[width * height];
+        image.CopyPixelDataTo(MemoryMarshal.Cast<int, Bgra32>(result));
+        return (result, width, height);
+    }
+
+   void Init()
+    {
+        wave = new bool[MX * MY][];
+        compatible = new int[wave.Length][][];
+        for (int i = 0; i < wave.Length; i++)
+        {
+            wave[i] = new bool[T];
+            compatible[i] = new int[T][];
+            for (int t = 0; t < T; t++) compatible[i][t] = new int[4];
+        }
