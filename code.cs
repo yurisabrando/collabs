26ce14401357de29e0463dd7dcc2e4c995e61a32
Edmund Gerhardt <162111400+yurisabrando@users.noreply.github.com>
2024-03-04 11:33:08 +0200

    Update code.cs

--- code.cs
+++ code.cs
@@ -18,3 +18,19 @@
             for (int t = 0; t < T; t++) compatible[i][t] = new int[4];
         }
 //hello
+ public bool Run(int seed, int limit)
+    {
+        if (wave == null) Init();
+
+        Clear();
+        Random random = new(seed);
+
+        for (int l = 0; l < limit || limit < 0; l++)
+        {
+            int node = NextUnobservedNode(random);
+            if (node >= 0)
+            {
+                Observe(node, random);
+                bool success = Propagate();
+                if (!success) return false;
+            }
